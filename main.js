9e1aebcfc4b927d279f069fdf03fe018374e349e
marcushoang888 <marcushoang.888@gmail.com>
2023-03-21 14:43:08 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+const height = document.querySelector(".height");
+
+const weight = document.querySelector(".weight");
+
+const btn = document.querySelector(".calculate")
+
+const bmiNumber = document.querySelector(".bmi-number")
+
+const bmiState = document.querySelector(".bmi-state")
+
+
+btn.addEventListener("click",function() {
+    const bmi = Number(weight.value) / Number(height.value)**2
+    console.log(bmi)
+
+    height.value = "";
+    weight.value ="";
+    bmiNumber.textContent = Math.round(bmi);
+
+    if (bmi <18) {
+        bmiState.textContent = "Suy Dinh Dưỡng"
+    } else if (bmi <=25) {
+        bmiState.textContent = "Bình Thường"
+    } else {
+        bmiState.textContent = "Béo Phì"
+        
+    }
+})
+
