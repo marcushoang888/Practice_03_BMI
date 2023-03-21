126e04e1fb3eaaa6c0cec75b4a8e83b70ac031c0
marcushoang888 <marcushoang.888@gmail.com>
2023-03-21 14:52:39 +0700

    optimize

--- main.js
+++ main.js
@@ -12,20 +12,23 @@ const bmiState = document.querySelector(".bmi-state")
 
 
 btn.addEventListener("click",function() {
-    const bmi = Number(weight.value) / (Number(height.value)/10**2)
-    console.log(bmi)
+    if (height.value!="" || weight.value!="") {
 
-    height.value = "";
-    weight.value ="";
-    bmiNumber.textContent =bmi.toFixed(2);
-
-    if (bmi <18) {
-        bmiState.textContent = "Suy Dinh Dưỡng"
-    } else if (bmi <=25) {
-        bmiState.textContent = "Bình Thường"
-    } else {
-        bmiState.textContent = "Béo Phì"
-        
+        const bmi = Number(weight.value) / (Number(height.value)/10**2)
+        console.log(bmi)
+    
+        height.value = "";
+        weight.value ="";
+        bmiNumber.textContent =bmi.toFixed(2);
+    
+        if (bmi <18) {
+            bmiState.textContent = "Suy Dinh Dưỡng"
+        } else if (bmi <=25) {
+            bmiState.textContent = "Bình Thường"
+        } else {
+            bmiState.textContent = "Béo Phì"
+            
+        }
     }
 })
 
