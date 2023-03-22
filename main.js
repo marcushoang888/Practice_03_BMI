51ac8afe6ff186e66a4c20467c82b071ca9a1df0
marcushoang888 <marcushoang.888@gmail.com>
2023-03-22 14:20:08 +0700

    crowning

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
-
-
 const height = document.querySelector(".height");
 
 const weight = document.querySelector(".weight");
 
-const btn = document.querySelector(".calculate")
+const btn = document.querySelector(".calculate");
 
-const bmiNumber = document.querySelector(".bmi-number")
+const bmiNumber = document.querySelector(".bmi-number");
 
-const bmiState = document.querySelector(".bmi-state")
+const bmiState = document.querySelector(".bmi-state");
 
+const alert = document.querySelector(".alert")
+function showResult(event) {
+    event.preventDefault();
+  if (height.value != "" && weight.value != "") {
+    const bmi = Number(weight.value) / (Number(height.value) / 10 ** 2);
+    console.log(bmi);
 
-btn.addEventListener("click",function() {
-    if (height.value!="" || weight.value!="") {
+    height.value = "";
+    weight.value = "";
+    bmiNumber.textContent = bmi.toFixed(2);
 
-        const bmi = Number(weight.value) / (Number(height.value)/10**2)
-        console.log(bmi)
-    
-        height.value = "";
-        weight.value ="";
-        bmiNumber.textContent =bmi.toFixed(2);
-    
-        if (bmi <18) {
-            bmiState.textContent = "Suy Dinh Dưỡng"
-        } else if (bmi <=25) {
-            bmiState.textContent = "Bình Thường"
-        } else {
-            bmiState.textContent = "Béo Phì"
-            
-        }
+    if (bmi < 18) {
+      bmiState.textContent = "Suy Dinh Dưỡng";
+    } else if (bmi <= 25) {
+      bmiState.textContent = "Bình Thường";
+    } else {
+      bmiState.textContent = "Béo Phì";
     }
-})
+  } else {
+    alert.style.display = "block"
+  }
+}
 
+btn.addEventListener("click", showResult);
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    showResult();
+  }
+});
